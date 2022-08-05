74d2877c053a4711362639f14c004f735dfc6992
Andrés Sánchez <diaverde77@gmail.com>
2022-08-04 22:22:35 -0500

    File Upload

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
-const Dishes = require('./models/dishes');
+//const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -30,6 +31,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -54,6 +65,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
