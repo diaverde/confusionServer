6ea5f9581a8f9e0838c0df522a903e388a9c5606
Andrés Sánchez <diaverde77@gmail.com>
2022-08-07 10:22:49 -0500

    Assignment 4 - Start

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '365185505619675',
-        clientSecret: 'a3e8a22f37debbb88ecd7a6be28251a6'
-    }
-}
